0a815b3a326e185417fe88a2bd6384f43322d705
parth <parth@advant.xyz>
2026-04-16 21:42:13 +0530

    add prisma supabase add and update user profile

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
+import { actionFunction } from "@/utils/types";
+const initialState = {
+  message: "",
+};
+
+function FormContainer({
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) {
+  const [state, formAction] = useFormState(action, initialState);
+
+  useEffect(() => {
+    if (state.message) {
+      toast(state.message);
+    }
+  }, [state]);
+
+  return <form action={formAction}>{children}</form>;
+}
+
+export default FormContainer;
--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { SignOutButton } from "@clerk/nextjs";
-import { useToast } from "../ui/use-toast";
+import { toast } from "sonner";
 
 function SignOutLink() {
-  const { toast } = useToast();
   const handleLogout = () => {
-    toast({ description: "You have been signed out." });
+    toast("You have been signed out.");
   };
 
   return (
