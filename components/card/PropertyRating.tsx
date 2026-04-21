61f51a8478dad2f3af34e9f193f0df6f5f15dec2
parth <parth@advant.xyz>
2026-04-21 14:11:42 +0530

    fix un used import and configs

--- components/card/PropertyRating.tsx
+++ components/card/PropertyRating.tsx
@@ -1,12 +1,6 @@
 import { FaStar } from "react-icons/fa";
 
-function PropertyRating({
-  propertyId,
-  inPage,
-}: {
-  propertyId: string;
-  inPage: boolean;
-}) {
+function PropertyRating({ inPage }: { inPage: boolean }) {
   // temp
   const rating = 4.7;
   const count = 100;
