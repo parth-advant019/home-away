dfd8335c0f2667d561f70d2baa9fe850d54c71d6
parth <parth@advant.xyz>
2026-04-21 13:43:47 +0530

    complete details page

--- components/card/CountryFlagAndName.tsx
+++ components/card/CountryFlagAndName.tsx
@@ -8,9 +8,9 @@ function CountryFlagAndName({ countryCode }: { countryCode: string }) {
       ? `${validCountry!.name.substring(0, 20)}...`
       : validCountry!.name;
   return (
-    <span className="flex justify-between items-center gap-2 text-sm">
+    <span className="flex justify-start items-center gap-2 text-sm">
       <ReactCountryFlag countryCode={countryCode} svg />
-      {validCountry?.flag} {countryName}
+      {countryName}
     </span>
   );
 }
